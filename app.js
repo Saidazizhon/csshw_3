0befda8e6ffb2c64e8e193027246684ca401d319
Saidazizhon <asrarovs26@gmail.com>
2024-07-20 15:48:18 +0500

    a

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const skeleton =  document.querySelector(".skeleton")
 const wrapper = document.querySelector(".wrapper")
 const seeMoreBtn = document.querySelector(".see-more__btn")
 const collection = document.querySelector(".collection")
+const gam_card = document.querySelector(".gam_card")
 
 const API_URL = "https://dummyjson.com"
 
+function gam_cardOpen(){
+    gam_card.classList.toggle("show")
+}
 for(let i=0; i<12; i++){
     let skeletonItem = document.createElement("div")
     skeletonItem.classList.add("skeleton__item")
